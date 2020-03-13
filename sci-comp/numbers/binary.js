04957931a5f6a8ce44daad582bc73a28f7461498
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2020-03-13 16:25:43 +0000

    Fixed writing to double to buffer (JS)

--- sci-comp/numbers/binary.js
+++ sci-comp/numbers/binary.js
@@ -7,13 +7,14 @@ const rl = readline.createInterface({
 
 function binary(num) {
     const buf=Buffer.alloc(8); // 8 bytes needed for double
-    buf.writeFloatBE(num);
+    buf.writeDoubleBE(num);
     return Array.from(buf.values()).map(b => b.toString(2).padStart(8,'0')).join();
 }
 
 function main_loop() {
-    rl.question('Enter a number: ', (num) => {
-        console.log(binary(parseFloat(num)));
+    rl.question('Enter a number: ', (line) => {
+        const num = parseFloat(line);
+        console.log(binary(num));
         main_loop();
     });
 };
