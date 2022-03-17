6de89fc7fc37315f00d992085b7b989941220553
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2022-03-17 19:15:12 +0000

    Calculating 1d derivative (numerical errors are expected)

--- sandbox/cuda/add.cu
+++ sandbox/cuda/add.cu
@@ -3,16 +3,16 @@
 
 // Kernel function to add the elements of two arrays
 __global__
-void add(int n, float *x, float *y)
+void add(int n, float dx, float *x, float *y)
 { 
   int delta = n / (blockDim.x * gridDim.x) + 1;
   int start = delta*(blockIdx.x * blockDim.x + threadIdx.x);
   int end = start + delta;
-  if (end>n) {
-    end = n;
+  if (end >= n) {
+    end = n - 1;
   }
   for (int i = start; i < end; ++i) {
-    y[i] = x[i] + y[i];
+    y[i] = (x[i+1] - x[i]) / dx;
   }
 }
 
@@ -20,6 +20,7 @@ int main(void)
 {
   int N = 1<<26;
   float *x, *y;
+  float dx = float(2*M_PI/N);
 
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&x, N*sizeof(float));
@@ -27,22 +28,27 @@ int main(void)
 
   // initialize x and y arrays on the host
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+    x[i] = sin(i*dx);
+    y[i] = 0.0f;
   }
 
   // Run kernel on 1M elements on the GPU
   int blockSize = 256;
   int numBlocks = (N + blockSize - 1) / blockSize;
-  add<<<numBlocks, blockSize>>>(N, x, y);
+  add<<<numBlocks, blockSize>>>(N, dx, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
 
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
+
+  for (int i = 0; i < N-1; i++) {
+    maxError = fmax(maxError, fabs(y[i]-cos((i + 0.5f)*dx)));
+//    std::cout << i*dx << " " << y[i] << " " << maxError << std::endl;
+  }
+
+
   std::cout << "Max error: " << maxError << std::endl;
 
   // Free memory
