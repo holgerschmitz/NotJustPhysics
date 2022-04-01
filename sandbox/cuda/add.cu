527720508fa76cdb1ce26a870899c3b14027d1f5
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2022-04-01 09:03:40 +0100

    Checking with a simpler function on a smaller array

--- sandbox/cuda/add.cu
+++ sandbox/cuda/add.cu
@@ -9,7 +9,7 @@ void add(int a0, int a1, int s0, int s1, int d0, int d1, float dx, float *x, flo
   int start = delta*(blockIdx.x * blockDim.x + threadIdx.x);
   int end = start + delta;
   if (end >= a0*a1) {
-    end =  a0*a1 - 1;
+    end =  a0*a1;
   }
   
   int p0 = start % a0 + s0;
@@ -24,7 +24,7 @@ void add(int a0, int a1, int s0, int s1, int d0, int d1, float dx, float *x, flo
   int i = start;
   while (i < end) {
     
-    y[j] = (x[j+1] + x[j-1] + x[j+d0] + x[j-d0] - 4*x[j]) / (dx*dx);
+    y[j] = x[j]; //(x[j+1] + x[j-1] + x[j+d0] + x[j-d0] - 4*x[j]) / (dx*dx);
     
     ++i;
     if (++j >= t0) {
@@ -33,12 +33,19 @@ void add(int a0, int a1, int s0, int s1, int d0, int d1, float dx, float *x, flo
   }
 }
 
+template<typename T>
+T testFunc(T x, T y) {
+//  float r2 = x*x + y*y;
+//  return exp(-r2);
+    return 10000*x + y;
+}
+
 int main(void)
 {
-  int D = 1<<10;
+  int D = 1<<4;
   int N = D*D;
   float *x, *y;
-  float dx = float(4.0f/D);
+  float dx = 1.0f; // float(4.0f/D);
 
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&x, N*sizeof(float));
@@ -47,8 +54,7 @@ int main(void)
   // initialize x and y arrays on the host
   for (int i = 0; i < D; ++i) {
     for (int j = 0; j < D; ++j) {
-      float r2 = (i*i + j*j)*dx*dx;
-      x[i*D + j] = exp(-r2);
+      x[i*D + j] = testFunc(i*dx, j*dx);
     }
   }
 
@@ -64,15 +70,16 @@ int main(void)
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
 
-  // Check for errors (all values should be 3.0f)
+  // Check for errors
   float maxError = -1.0f;
   int maxI, maxJ;
 
   for (int i = 0; i < D; ++i) {
     for (int j = 0; j < D; ++j) {
-      float r2 = (i*i + j*j)*dx*dx;
-      float expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : 4.0f*(r2 - 1)*exp(-r2);
-      
+//      float r2 = (i*i + j*j)*dx*dx;
+//      float expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : 4.0f*(r2 - 1)*exp(-r2);
+      float expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : testFunc(i*dx, j*dx);
+    
       float err = fabs(y[i*D + j] - expected);
       if (err > maxError) {
         maxError = err;
@@ -85,6 +92,24 @@ int main(void)
 
   std::cout << "Max error: " << maxError << " " << maxI << " " << maxJ << " " << y[maxI*D + maxJ] << std::endl;
 
+  float xf = maxI*dx;
+  float yf = maxJ*dx;
+
+  float expectedAtMax = testFunc(xf, yf); 
+// (testFunc(xf - dx, yf) + testFunc(xf + dx, yf) + testFunc(xf, yf - dx) + testFunc(xf, yf + dx)
+//    - 4.0*testFunc(xf, yf))/(dx*dx);
+
+  std::cout << "Expected value: " << expectedAtMax << std::endl;
+
+  // double check, get it? ;)
+  double xd = maxI*dx;
+  double yd = maxJ*dx;
+  
+  double precise = testFunc(xd, yd); 
+// (testFunc(xd - dx, yd) + testFunc(xd + dx, yd) + testFunc(xd, yd - dx) + testFunc(xd, yd + dx) 
+//    - 4.0*testFunc(xd, yd))/(double(dx)*double(dx));
+
+  std::cout << "Better value: " << precise << std::endl;
   // Free memory
   cudaFree(x);
   cudaFree(y);
