48252bc96412e129cebd08cc8ca5be5b80180d16
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2022-04-28 16:17:47 +0100

    Tried creating an iterator like structure but now I completely messed up

--- sandbox/cuda/add.cu
+++ sandbox/cuda/add.cu
@@ -13,7 +13,7 @@ struct GridStride {
   int outerDim[rank];
 
   __host__ __device__
-  int getInnerCount() {
+  int getInnerCount() const {
     int result = innerDim[0];
     for (int i=1; i<rank; ++i) {
       result *= innerDim[i];
@@ -22,7 +22,7 @@ struct GridStride {
   }
 
   __host__ __device__
-  void innerPosFromInnerCount(int count, int (&pos)[rank]) {
+  void innerPosFromInnerCount(int count, int (&pos)[rank]) const {
     for (int i=rank-1; i>=0; --i) {
       pos[i] = count % innerDim[i];
       count = count / innerDim[i];
@@ -30,7 +30,7 @@ struct GridStride {
   }
 
   __host__ __device__
-  int outerCountFromInnerPos(int pos[rank]) {
+  int outerCountFromInnerPos(int pos[rank]) const {
     int count = pos[0] + offset[0];
     for (int i=1; i<rank; ++i) {
       count = count*outerDim[i-1] + pos[i] + offset[i];
@@ -39,34 +39,53 @@ struct GridStride {
   }
 };
 
+template<int rank>
+struct LocalGridIterator {
+  int nIter;
+  int delta;
+  int start;
+  int end;
+  int i;
+  int j;
+  int skip0;
+  int t0;
+
+  __device__
+  LocalGridIterator(const GridStride<rank> &stride) {
+    nIter = stride.getInnerCount();
+    delta = nIter / (blockDim.x * gridDim.x) + 1;
+    start = delta*(blockIdx.x * blockDim.x + threadIdx.x);
+    end = start + delta;
+    if (end >= nIter) {
+      end =  nIter;
+    }
+
+    skip0 = stride.outerDim[0] - stride.offset[0] - stride.innerDim[0];
+    t0 = stride.offset[0] + stride.innerDim[0];
+
+    int innerPos[2];
+    stride.innerPosFromInnerCount(start, innerPos);
+
+    j = stride.outerCountFromInnerPos(innerPos);
+    i = start;
+  }
+};
+
 // Kernel function to add the elements of two arrays
 __global__
 void add(GridStride<2> stride, float dx, float *x, float *y)
 { 
-  int nIter = stride.getInnerCount();
-  int delta = nIter / (blockDim.x * gridDim.x) + 1;
-  int start = delta*(blockIdx.x * blockDim.x + threadIdx.x);
-  int end = start + delta;
-  if (end >= nIter) {
-    end =  nIter;
-  }
-  
-  int innerPos[2];
-  stride.innerPosFromInnerCount(start, innerPos);
+  LocalGridIterator<2> iter(stride);
 
-  int skip0 = stride.outerDim[0] - stride.offset[0] - stride.innerDim[0];
-  int t0 = stride.offset[0] + stride.innerDim[0];
-
-  int j = stride.outerCountFromInnerPos(innerPos);
-
-  int i = start;
-  while (i < end) {
+  int k = 0;
+  while (iter.i < iter.end) {
     
-    y[j] = (x[j+1] + x[j-1] + x[j+stride.outerDim[0]] + x[j-stride.outerDim[0]] - 4*x[j]) / (dx*dx); 
+    y[iter.j] = iter.j; // (x[iter.j+1] + x[iter.j-1] + x[iter.j+stride.outerDim[0]] + x[iter.j-stride.outerDim[0]] - 4*x[iter.j]) / (dx*dx); 
     
-    ++i;
-    if (++j >= t0) {
-      j += skip0 - 1;
+    ++k;
+    ++iter.i;
+    if (++iter.j >= iter.t0) {
+      iter.j += iter.skip0 - 1;
     }
   }
 }
@@ -80,10 +99,10 @@ T testFunc(T x, T y) {
 
 int main(void)
 {
-  int D = 1<<10;
+  int D = 20;
   int N = D*D;
   float *x, *y;
-  float dx = float(4.0f/D);
+  float dx = float(0.5f/D);
 
   GridStride<2> stride{{D - 8, D - 8}, {4, 4}, {D, D}};
 
@@ -105,7 +124,7 @@ int main(void)
   // Run kernel on 1M elements on the GPU
   int blockSize = 256;
   int numBlocks = (N + blockSize - 1) / blockSize;
-  add<<<numBlocks, blockSize>>>(stride, dx, x, y);
+  add<<<1, 1>>>(stride, dx, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
@@ -132,34 +151,35 @@ int main(void)
         maxI = i;
         maxJ = j;
       }
-//      std::cout << i*dx << " " << j*dx << " " << y[i*D + j] << std::endl;
+      std::cout << i*dx << " " << j*dx << " " << y[i*D + j] << std::endl;
     }
+    std::cout << std::endl;
   }
 
-  std::cout << "Max error: " << maxError << " " << maxI << " " << maxJ << " " << y[maxI*D + maxJ] << " " << comparedTo << std::endl;
+  std::cerr << "Max error: " << maxError << " " << maxI << " " << maxJ << " " << y[maxI*D + maxJ] << " " << comparedTo << std::endl;
 
-  float xf = maxI*dx;
-  float yf = maxJ*dx;
+//   float xf = maxI*dx;
+//   float yf = maxJ*dx;
 
-  float expectedAtMax = // testFunc(xf - dx, yf); 
-( testFunc(xf - dx, yf) + testFunc(xf + dx, yf) + 
-  testFunc(xf, yf - dx) + testFunc(xf, yf + dx)
-  - 4.0*testFunc(xf, yf)
-)/(dx*dx);
+//   float expectedAtMax = // testFunc(xf - dx, yf); 
+// ( testFunc(xf - dx, yf) + testFunc(xf + dx, yf) + 
+//   testFunc(xf, yf - dx) + testFunc(xf, yf + dx)
+//   - 4.0*testFunc(xf, yf)
+// )/(dx*dx);
 
-  std::cout << "Expected value: " << expectedAtMax << std::endl;
+//   std::cout << "Expected value: " << expectedAtMax << std::endl;
 
-  // double check, get it? ;)
-  double xd = maxI*dx;
-  double yd = maxJ*dx;
+//   // double check, get it? ;)
+//   double xd = maxI*dx;
+//   double yd = maxJ*dx;
   
-  double precise = // testFunc(xd - dx, yd); 
-( testFunc(xd - dx, yd) + testFunc(xd + dx, yd) + 
-  testFunc(xd, yd - dx) + testFunc(xd, yd + dx) 
-  - 4.0*testFunc(xd, yd)
-)/(double(dx)*double(dx));
+//   double precise = // testFunc(xd - dx, yd); 
+// ( testFunc(xd - dx, yd) + testFunc(xd + dx, yd) + 
+//   testFunc(xd, yd - dx) + testFunc(xd, yd + dx) 
+//   - 4.0*testFunc(xd, yd)
+// )/(double(dx)*double(dx));
 
-  std::cout << "Better value: " << precise << std::endl;
+//   std::cout << "Better value: " << precise << std::endl;
   // Free memory
   cudaFree(x);
   cudaFree(y);
