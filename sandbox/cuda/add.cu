f732870470bead391355a9ce4135c8e475958846
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2022-04-29 11:09:52 +0100

    Switched to double precision

--- sandbox/cuda/add.cu
+++ sandbox/cuda/add.cu
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
 
+typedef double real;
+
 template<int rank>
 struct Array {
   int val[rank];
@@ -80,7 +82,7 @@ struct LocalGridIterator {
 
 // Kernel function to add the elements of two arrays
 __global__
-void add(GridStride<2> stride, float dx, float *x, float *y)
+void add(GridStride<2> stride, real dx, real *x, real *y)
 { 
   LocalGridIterator<2> iter(stride);
 
@@ -101,23 +103,23 @@ void add(GridStride<2> stride, float dx, float *x, float *y)
 
 template<typename T>
 T testFunc(T x, T y) {
-  float r2 = x*x + y*y;
+  real r2 = x*x + y*y;
   return exp(-r2);
 //    return 10000*x + y;
 }
 
 int main(void)
 {
-  int D = 20;
+  int D = 200;
   int N = D*D;
-  float *x, *y;
-  float dx = float(0.5f/D);
+  real *x, *y;
+  real dx = real(0.5f/D);
 
   GridStride<2> stride{{D - 8, D - 8}, {4, 4}, {D, D}};
 
   // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
+  cudaMallocManaged(&x, N*sizeof(real));
+  cudaMallocManaged(&y, N*sizeof(real));
 
   // initialize x and y arrays on the host
   for (int i = 0; i < D; ++i) {
@@ -133,27 +135,27 @@ int main(void)
   // Run kernel on 1M elements on the GPU
   int blockSize = 256;
   int numBlocks = (N + blockSize - 1) / blockSize;
-  add<<<3, 1>>>(stride, dx, x, y);
+  add<<<numBlocks, blockSize>>>(stride, dx, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
 
   // Check for errors
-  float maxError = -1.0f;
-  float comparedTo;
+  real maxError = -1.0f;
+  real comparedTo;
   int maxI, maxJ;
 
   for (int i = 0; i < D; ++i) {
     for (int j = 0; j < D; ++j) {
-//      float r2 = (i*i + j*j)*dx*dx;
-//      float expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : 4.0f*(r2 - 1)*exp(-r2);
-      float expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : (
-        testFunc<float>(i*dx - dx, j*dx) + testFunc<float>(i*dx + dx, j*dx) + 
-        testFunc<float>(i*dx, j*dx - dx) + testFunc<float>(i*dx, j*dx + dx) 
+//      real r2 = (i*i + j*j)*dx*dx;
+//      real expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : 4.0f*(r2 - 1)*exp(-r2);
+      real expected = (i<4 || j<4 || i>=D-4 || j>=D-4) ? 0.0f : (
+        testFunc<real>(i*dx - dx, j*dx) + testFunc<real>(i*dx + dx, j*dx) + 
+        testFunc<real>(i*dx, j*dx - dx) + testFunc<real>(i*dx, j*dx + dx) 
         - 4.0*testFunc(i*dx, j*dx)
       ) /(dx*dx);
     
-      float err = fabs(y[i*D + j] - expected);
+      real err = fabs(y[i*D + j] - expected);
       if (err > maxError) {
         maxError = err;
         comparedTo = expected;
@@ -167,10 +169,10 @@ int main(void)
 
   std::cerr << "Max error: " << maxError << " " << maxI << " " << maxJ << " " << y[maxI*D + maxJ] << " " << comparedTo << std::endl;
 
-//   float xf = maxI*dx;
-//   float yf = maxJ*dx;
+//   real xf = maxI*dx;
+//   real yf = maxJ*dx;
 
-//   float expectedAtMax = // testFunc(xf - dx, yf); 
+//   real expectedAtMax = // testFunc(xf - dx, yf); 
 // ( testFunc(xf - dx, yf) + testFunc(xf + dx, yf) + 
 //   testFunc(xf, yf - dx) + testFunc(xf, yf + dx)
 //   - 4.0*testFunc(xf, yf)
