09b14ad5dd1188b487d5b40771957993a2147f10
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2023-04-17 13:56:36 +0100

    Started implementing Lagrangian interpolaion

--- sci-comp/01-basics/interpolation/interpolation.py
+++ sci-comp/01-basics/interpolation/interpolation.py
@@ -2,6 +2,9 @@
 import math
 import numpy as np
 
+def isEven(n: int):
+  return n % 2 == 0
+
 def interpol_piecewise(data: np.ndarray, xmin: float, xmax: float):
   h = (xmax - xmin) / (data.shape[0] - 1)
   def interp_func(x):
@@ -16,6 +19,43 @@ def interpol_linear(data: np.ndarray, xmin: float, xmax: float):
   def interp_func(x):
     xp = x-xmin
     nn = math.floor(xp/h)
+    if (nn==L):
+      return data[nn]
+    
+
+    alpha = xp/h - nn
+    return data[nn]*(1-alpha) + data[nn+1]*alpha
+  
+  return interp_func
+
+def lagrange_polynomial(data: np.ndarray, xlo: float, h: float):
+  N = data.shape[0]
+
+  def interp_polynomial(x: float):
+    sum = 0.0
+    for i in range(N):
+      prod = 1.0
+      xi = xlo + i*h
+      for j in range(N):
+        if i==j:
+          continue
+        xj = xlo + j*h
+        prod = prod * (x - xj)/((i-j)*h)
+      sum = sum + prod
+    return sum
+
+def interpol_lagrange(data: np.ndarray, order: int, xmin: float, xmax: float):
+  L = data.shape[0] - 1
+  h = (xmax - xmin) / L
+  oh = math.floor(order / 2)
+  def interp_func(x):
+    xp = x-xmin
+    if (isEven(order)):
+      nn = round(xp/h)
+    else:
+      nn = math.floor(xp/h)
+    
+    nstart = nn - oh
     if (nn==L):
       return data[nn]
     
